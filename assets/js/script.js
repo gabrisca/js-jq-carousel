9210dade49680447c9782994107b0c11590b6ee4
gabrisca <gabrielescarparo1985@gmail.com>
2021-04-20 18:26:25 +0200

    prettier

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
-$(function(){
+$(function () {
   // REFERENCES
   // // creo una referenza al f_awsome da cliccare per scorrere in avanti
   var next = $(".carousel_right");
   // console.log(next);
 
   // // creo una referenza al f_awsome da cliccare per scorrere indietro
-  var back = $(".carousel_left")
+  var back = $(".carousel_left");
   // console.log(back);
 
   // EVENTS
@@ -15,25 +15,24 @@ $(function(){
   // FUNCTIONS
 
   // funzione per scorre in avanti le foto
-  function nextImage(){
+  function nextImage() {
     // creo una referenza all'immagine attiva
     var activeImg = $(".carousel_center img.active");
     console.log(activeImg);
     // creo una referenza al circle attivo
     var activeCircle = $(".nav_carousel i.active");
     console.log(activeCircle);
-    
+
     // rimuovo la classe active (che ha display inline-block) a activeImg e activeCircle
     activeImg.removeClass("active");
     activeCircle.removeClass("active");
 
-    
     // se l'elemento ha classe last attivo l'elemento con classe first (la prima foto)
-    if(activeImg.hasClass("last")){
+    if (activeImg.hasClass("last")) {
       // aggiungo active a first
       $(".carousel_center img.first").addClass("active");
       $(".nav_carousel i.first").addClass("active");
-    }else{
+    } else {
       // aggiungo la classe active (che ha display inline-block) all'immagine e al circle successiva
       activeImg.next("img").addClass("active");
       activeCircle.next("i").addClass("active");
@@ -41,7 +40,7 @@ $(function(){
   }
 
   // funzione per scorre indietro le foto
-  function backImage(){
+  function backImage() {
     // creo una referenza all'immagine attiva
     var activeImg = $(".carousel_center img.active");
     console.log(activeImg);
@@ -53,9 +52,8 @@ $(function(){
     activeImg.removeClass("active");
     activeCircle.removeClass("active");
 
-    
     // se l'elemento ha classe first attivo l'elemento con classe last (la prima foto)
-    if(activeImg.hasClass("first")){
+    if (activeImg.hasClass("first")) {
       // aggiungo active a first
       $(".carousel_center img.last").addClass("active");
       $(".nav_carousel i.first").addClass("active");
@@ -65,6 +63,4 @@ $(function(){
       activeCircle.prev("i").addClass("active");
     }
   }
-
-})
-
+});
