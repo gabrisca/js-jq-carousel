45f7a2776a6a18398393c9597af4f84f45789568
gabrisca <gabrielescarparo1985@gmail.com>
2021-04-20 17:59:18 +0200

    funzioni next e back

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,16 +8,17 @@ $(function(){
   var back = $(".carousel_left")
   // console.log(back);
 
-  // creo una referenza all'immagine attiva
-  var activeImg = $(".carousel_center img.active");
-  console.log(activeImg);
-
   // EVENTS
   next.click(nextImage);
   back.click(backImage);
 
   // FUNCTIONS
+
+  // funzione per scorre in avanti le foto
   function nextImage(){
+    // creo una referenza all'immagine attiva
+    var activeImg = $(".carousel_center img.active");
+    console.log(activeImg);
     
     // rimuovo la classe active (che ha display inline-block) a activeImg
     activeImg.removeClass("active");
@@ -25,11 +26,30 @@ $(function(){
     // aggiungo la classe active (che ha display inline-block) all'immagine successiva
     activeImg.next().addClass("active");
     
-
+    // se l'elemento ha classe last attivo l'elemento con classe first (la prima foto)
+    if(activeImg.hasClass("last")){
+      // aggiungo active a first
+      $(".carousel_center img.first").addClass("active");
+    }
   }
 
+  // funzione per scorre indietro le foto
   function backImage(){
-    console.log("back");
+    // creo una referenza all'immagine attiva
+    var activeImg = $(".carousel_center img.active");
+    console.log(activeImg);
+    
+    // rimuovo la classe active (che ha display inline-block) a activeImg
+    activeImg.removeClass("active");
+
+    // aggiungo la classe active (che ha display inline-block) all'immagine precedente
+    activeImg.prev().addClass("active");
+    
+    // se l'elemento ha classe first attivo l'elemento con classe last (la prima foto)
+    if(activeImg.hasClass("first")){
+      // aggiungo active a first
+      $(".carousel_center img.last").addClass("active");
+    }
   }
 
 })
