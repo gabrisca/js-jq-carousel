879a04174acfde11ddc38f5dde94a5854f011447
gabrisca <gabrielescarparo1985@gmail.com>
2021-04-20 17:46:07 +0200

    creo referenze next e back

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,36 @@
 $(function(){
-  var next = $("carousel_right");
-  console.log(next, "next");
-  var back = $("carousel_left")
-  console.log(back, "back");
+  // REFERENCES
+  // // creo una referenza al f_awsome da cliccare per scorrere in avanti
+  var next = $(".carousel_right");
+  // console.log(next);
+
+  // // creo una referenza al f_awsome da cliccare per scorrere indietro
+  var back = $(".carousel_left")
+  // console.log(back);
+
+  // creo una referenza all'immagine attiva
+  var activeImg = $(".carousel_center img.active");
+  console.log(activeImg);
+
+  // EVENTS
+  next.click(nextImage);
+  back.click(backImage);
+
+  // FUNCTIONS
+  function nextImage(){
+    
+    // rimuovo la classe active (che ha display inline-block) a activeImg
+    activeImg.removeClass("active");
+
+    // aggiungo la classe active (che ha display inline-block) all'immagine successiva
+    activeImg.next().addClass("active");
+    
+
+  }
+
+  function backImage(){
+    console.log("back");
+  }
 
 })
 
