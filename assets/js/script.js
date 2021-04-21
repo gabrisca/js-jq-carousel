6b6c95fbbda0081bd32dcf17a6df18fb6d14dcad
gabrisca <gabrielescarparo1985@gmail.com>
2021-04-21 11:06:10 +0200

    option 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,8 +30,8 @@ $(function () {
     // se l'elemento ha classe last attivo l'elemento con classe first (la prima foto)
     if (activeImg.hasClass("last")) {
       // aggiungo active a first
-      $(".carousel_center img.first").addClass("active");
-      $(".nav_carousel i.first").addClass("active");
+      $(".carousel_center img").first().addClass("active");
+      $(".nav_carousel i").first().addClass("active");
     } else {
       // aggiungo la classe active (che ha display inline-block) all'immagine e al circle successiva
       activeImg.next("img").addClass("active");
@@ -55,12 +55,33 @@ $(function () {
     // se l'elemento ha classe first attivo l'elemento con classe last (la prima foto)
     if (activeImg.hasClass("first")) {
       // aggiungo active a first
-      $(".carousel_center img.last").addClass("active");
-      $(".nav_carousel i.first").addClass("active");
+      $(".carousel_center img").last().addClass("active");
+      $(".nav_carousel i").last().addClass("active");
     } else {
       // aggiungo la classe active (che ha display inline-block) all'immagine  e al circle precedente
       activeImg.prev("img").addClass("active");
       activeCircle.prev("i").addClass("active");
     }
   }
+
+  // // inserire circle da js (opzione2). Eliminare da html gli elementi <i class="fas fa-circle"></i> poi...
+  // $(".carousel_center img").each(function(index){
+  //   console.log("index: " + index);
+  //   var navCarousel = $(".nav_carousel");
+  //   var active = "";
+  //   if(index === 0) active = "active";
+  //   var tag = '<i class="fas fa-circle ' + active + '"></i>';
+  //   navCarousel.append(tag);
+  // });
+
+  // // aggiungere un listenere "click" a tutti i circle
+  // $(".nav_carousel i").on("click", function(){
+  //   var index = $(this).index();
+  //   console.log("index " + index);
+  //   $(".nav_carousel i.active").removeClass("active");
+  //   $(".carousel_center img.active").removeClass("active");
+  //   $(".carousel_center img").eq(index).addClass("active");
+  //   $(this).addClass("active");
+  // });
+
 });
